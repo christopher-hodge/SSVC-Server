1aa1762132c8c0af91927c6b4b7940a10bd1643d
veggieinhd <christopher.matthew.hodge@gmail.com>
2026-03-27 23:16:47 -0500

    xd

--- internal/crafting/service/apply_affix.go
+++ internal/crafting/service/apply_affix.go
@@ -1,30 +1,34 @@
 package service
 
-import "SSVC-Server/internal/crafting/domain"
+import (
+	"SSVC-Server/internal/crafting/domain"
+	"errors"
+)
 
-func ApplyAffix(
-	ctx *domain.CraftingContext,
-	affixType domain.AffixType,
-) error {
-
+func ApplyAffixLogic(ctx *domain.CraftingContext, affixType domain.AffixType) error {
 	limits := domain.AffixLimitsByRarity[ctx.Item.Rarity]
 
-	if err := domain.CanAddAffix(ctx.Item, affixType); err != nil {
-		return err
+	if affixType == domain.Both || affixType == domain.All {
+		prefixAvailable := len(ctx.Item.Prefixes) < limits.MaxPrefixes
+		suffixAvailable := len(ctx.Item.Suffixes) < limits.MaxSuffixes
+
+		switch {
+		case prefixAvailable && suffixAvailable:
+			affixType = domain.AffixType(ctx.RNG.Intn(2)) // pick randomly
+		case prefixAvailable:
+			affixType = domain.Prefix
+		case suffixAvailable:
+			affixType = domain.Suffix
+		default:
+			return errors.New("no affix slots available")
+		}
 	}
 
-	//Only checks if the affixType is Both, then sets it to a vaild affix
-	if affixType == domain.Both {
-		switch {
-		case len(ctx.Item.Prefixes) < limits.MaxPrefixes && len(ctx.Item.Suffixes) < limits.MaxSuffixes:
-			affixType = domain.AffixType(ctx.RNG.Intn(2))
-
-		case len(ctx.Item.Prefixes) < limits.MaxPrefixes:
-			affixType = domain.Prefix
-
-		default:
-			affixType = domain.Suffix
-		}
+	if affixType == domain.Prefix && len(ctx.Item.Prefixes) >= limits.MaxPrefixes {
+		return errors.New("max prefixes reached")
+	}
+	if affixType == domain.Suffix && len(ctx.Item.Suffixes) >= limits.MaxSuffixes {
+		return errors.New("max suffixes reached")
 	}
 
 	affix, err := RollAffix(ctx, affixType)
@@ -32,12 +36,39 @@ func ApplyAffix(
 		return err
 	}
 
-	item := ctx.Item
-
 	if affixType == domain.Prefix {
-		item.Prefixes = append(item.Prefixes, affix)
+		ctx.Item.Prefixes = append(ctx.Item.Prefixes, affix)
 	} else {
-		item.Suffixes = append(item.Suffixes, affix)
+		ctx.Item.Suffixes = append(ctx.Item.Suffixes, affix)
+	}
+
+	return nil
+}
+
+func ApplyMultipleAffixesLogic(
+	ctx *domain.CraftingContext,
+	affixType domain.AffixType,
+	count int,
+) error {
+
+	for i := 0; i < count; i++ {
+		switch affixType {
+
+		case domain.OnlyPrefixes:
+			if err := ApplyAffixLogic(ctx, domain.Prefix); err != nil {
+				return err
+			}
+
+		case domain.OnlySuffixes:
+			if err := ApplyAffixLogic(ctx, domain.Suffix); err != nil {
+				return err
+			}
+
+		case domain.All:
+			if err := ApplyAffixLogic(ctx, domain.Both); err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
--- internal/crafting/service/catalyst.go
+++ internal/crafting/service/catalyst.go
@@ -9,56 +9,100 @@ import (
 type ImbuementCatalyst struct{}
 type ReconstructionCatalyst struct{}
 type ElevatingCatalyst struct{}
+type DefiantCatalyst struct{}
+type AscendantCatalyst struct{}
+
+type CraftStep func(ctx *domain.CraftingContext) error
+
+func ExecutePipeline(ctx *domain.CraftingContext, steps []CraftStep) error {
+	for _, step := range steps {
+		if err := step(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func RequireRarity(r domain.Rarity) CraftStep {
+	return func(ctx *domain.CraftingContext) error {
+		if ctx.Item.Rarity != r {
+			return errors.New("Invalid item rarity.")
+		}
+		return nil
+	}
+}
+
+func ClearAffixes() CraftStep {
+	return func(ctx *domain.CraftingContext) error {
+		ctx.Item.Prefixes = []domain.AffixInstance{}
+		ctx.Item.Suffixes = []domain.AffixInstance{}
+		return nil
+	}
+}
+
+func SetRarity(r domain.Rarity) CraftStep {
+	return func(ctx *domain.CraftingContext) error {
+		ctx.Item.Rarity = r
+		return nil
+	}
+}
+
+func AddAffix(affixType domain.AffixType) CraftStep {
+	return func(ctx *domain.CraftingContext) error {
+		return ApplyAffixLogic(ctx, affixType)
+	}
+}
+
+func AddAffixes(count int, affixType domain.AffixType) CraftStep {
+	return func(ctx *domain.CraftingContext) error {
+		for i := 0; i < count; i++ {
+			if err := ApplyAffixLogic(ctx, affixType); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
 
 func (c *ImbuementCatalyst) Apply(ctx *domain.CraftingContext, affixType domain.AffixType) error {
-
-	item := ctx.Item
-
-	if item.Rarity != domain.Normal {
-		return errors.New("Item must be Normal.")
-	}
-
-	item.Rarity = domain.Magic
-
-	// Determines if this is a prefix (0) or suffix (1)
-	newAffixType := ctx.RNG.Intn(2)
-
-	return ApplyAffix(ctx, domain.AffixType(newAffixType))
+	return ExecutePipeline(ctx, []CraftStep{
+		RequireRarity(domain.Normal),
+		SetRarity(domain.Magic),
+		AddAffix(domain.Both),
+	})
 }
 
 func (c *ReconstructionCatalyst) Apply(ctx *domain.CraftingContext) error {
+	count := ctx.RNG.Intn(2) + 1 // 1 or 2 mods
 
-	item := ctx.Item
-
-	if item.Rarity != domain.Normal && item.Rarity != domain.Magic {
-		return errors.New("Item must be Normal or Magic.")
-	}
-
-	if item.Rarity == domain.Normal {
-		item.Rarity = domain.Magic
-	}
-
-	item.Prefixes = []domain.AffixInstance{}
-	item.Suffixes = []domain.AffixInstance{}
-
-	//Determines if this is a prefix (0), suffix (1), or both(2)
-	newAffixType := ctx.RNG.Intn(3)
-
-	return ApplyAffix(ctx, domain.AffixType(newAffixType))
+	return ExecutePipeline(ctx, []CraftStep{
+		RequireRarity(domain.Magic),
+		ClearAffixes(),
+		AddAffixes(count, domain.All),
+	})
 }
 
 func (c *ElevatingCatalyst) Apply(ctx *domain.CraftingContext, affixType domain.AffixType) error {
+	return ExecutePipeline(ctx, []CraftStep{
+		RequireRarity(domain.Magic),
+		SetRarity(domain.Rare),
+		AddAffix(domain.Both),
+	})
+}
 
-	item := ctx.Item
+func (c *DefiantCatalyst) Apply(ctx *domain.CraftingContext, affixType domain.AffixType) error {
+	count := ctx.RNG.Intn(6) + 1
 
-	if item.Rarity != domain.Magic {
-		return errors.New("Item must be Magic.")
-	}
+	return ExecutePipeline(ctx, []CraftStep{
+		RequireRarity(domain.Rare),
+		ClearAffixes(),
+		AddAffixes(count, domain.All),
+	})
+}
 
-	item.Rarity = domain.Rare
-
-	// Determines if this is a prefix (0) or suffix (1)
-	newAffixType := ctx.RNG.Intn(2)
-
-	return ApplyAffix(ctx, domain.AffixType(newAffixType))
+func (c *AscendantCatalyst) Apply(ctx *domain.CraftingContext, affixType domain.AffixType) error {
+	return ExecutePipeline(ctx, []CraftStep{
+		RequireRarity(domain.Rare),
+		AddAffix(domain.Both),
+	})
 }
