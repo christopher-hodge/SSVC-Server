a4212f160bf6c7fd9b611bd447298b4df37e5450
veggieinhd <christopher.matthew.hodge@gmail.com>
2026-04-05 10:01:26 -0500

    consolidated logic into a single file

--- internal/crafting/service/apply_affix.go
+++ internal/crafting/service/apply_affix.go
@@ -90,3 +90,26 @@ func RollAffix(
 		MinLevel:       chosenAffix.MinLevel,
 	}, nil
 }
+
+func weightedRoll(
+	rng random.RNGer,
+	pool []domain.AffixDefinition,
+) domain.AffixDefinition {
+
+	totalWeight := 0
+	for _, def := range pool {
+		totalWeight += def.Weight
+	}
+
+	roll := rng.Intn(totalWeight)
+
+	running := 0
+	for _, def := range pool {
+		running += def.Weight
+		if roll < running {
+			return def
+		}
+	}
+
+	return pool[len(pool)-1]
+}
